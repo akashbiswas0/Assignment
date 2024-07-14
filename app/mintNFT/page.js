342f2ba1d023bddf56e80a23fa93cdc05b9be737
akashbiswas0 <akash8860biswas@gmail.com>
2024-07-15 04:35:48 +0530

    bug fix

--- app/mintNFT/page.js
+++ app/mintNFT/page.js
@@ -1,7 +1,29 @@
+"use client"
+import React, { useState } from 'react';
+import { mintLicenseFractions } from '@/utils';
+import Navbar from '@/components/Navbar';
 export default function Mintnft(){
+    const [licenseId, setLicenseId] = useState('');
+
+  const handleMintLicenseFractions = async () => {
+    await mintLicenseFractions(licenseId);
+  };
     return(
         <div>
-            mint nft
+            <Navbar />
+            <div className='mt-40 flex flex-col justify-center content-center'>
+            <h2>Mint License Fractions</h2>
+            <input
+            className='border-2 border-black w-24 rounded-md text-center'
+            type="text"
+            placeholder="License ID"
+            value={licenseId}
+            onChange={e => setLicenseId(e.target.value)}
+            />
+            
+            </div>
+            <button className='bg-blue-300' onClick={handleMintLicenseFractions}>Mint</button>
         </div>
     )
 }
+
--- app/purchaseFraction/page.js
+++ app/purchaseFraction/page.js
@@ -1,16 +1,10 @@
 "use client"
-import { useState, useEffect } from 'react';
-import { useAccount } from 'wagmi';
+
 import Navbar from '../../components/Navbar';
-import Input from '../../components/Input';
+import Input from '../../components/PurchaseInput';
 import Purchase from '../../components/Purchase';
 export default function PurchaseFraction() {
-    const { isConnected } = useAccount();
-    const [connected, setConnected] = useState(false);
-
-    useEffect(() => {
-        setConnected(isConnected);
-    }, [isConnected]);
+    
     return(
         <div>
             <Navbar />
@@ -18,9 +12,8 @@ export default function PurchaseFraction() {
                 <span className='text-black mt-40 text-center text-5xl tracking-normal font-semibold'>
                     Buy 1/10th Fractionalized NFT
                 </span>
-                <div className='border-2 border-gray-700 w-1/2 mx-auto mt-5 h-80 backdrop-blur-md bg-gray-200'>
-                    <Input />
-                    {connected && <Purchase />}
+                <div className=' w-1/2 mx-auto mt-5 h-80 backdrop-blur-md '>
+                    <Input />  
                 </div>
                 </div>
                 </div>
